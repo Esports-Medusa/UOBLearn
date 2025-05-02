2aa31921fa16c7a9b11e743500de261d1c09783c
Esports-Medusa <jakkiyuan@gmail.com>
2025-05-02 10:19:53 +0800

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,18 +1,18 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///uoblearn.db'
-
 
 db = SQLAlchemy(app)
 login = LoginManager()
 login.init_app(app)
 login.login_view = 'auth.login'
 
+migrate = Migrate(app, db)
 
 from app.routes import auth, courses, mentor, main
 app.register_blueprint(main.bp)
